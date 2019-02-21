a8d607adbfcc7ba1687f167dad000f004103bc08
Trent Going <trent.going@gmail.com>
2019-02-21 18:35:43 -0500

    Refactored for isolation of Queries

--- server/gql/Queries.js
+++ server/gql/Queries.js
@@ -53,10 +53,45 @@ function staffHelpRequests (parent, args, context, info) {
     });
 } 
 
+function helpdesks({staff_slack_id}, args, context, info) {
+  console.log(staff_slack_id);
+  return context.db.query({
+    text: 'SELECT * FROM helpdesk WHERE staff_slack_id = $1',
+    values: [staff_slack_id],
+  }) 
+    .then(result => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function helpdeskCount({staff_slack_id}, args, context, info) {
+  console.log(staff_slack_id);
+  return context.db.query({
+    text: 'SELECT COUNT(*) FROM helpdesk WHERE staff_slack_id = $1',
+    values: [staff_slack_id],
+  }) 
+    .then(result => {
+      return result.rows[0].count;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
 module.exports = {
-  hello,
-  allStaff,
-  staff,
-  allHelpRequests, 
-  staffHelpRequests
+  Query: {
+    hello,
+    allStaff,
+    staff,
+    allHelpRequests, 
+    staffHelpRequests
+  },
+  Staff: {
+    helpdesks,
+    helpdeskCount
+  }
 }
--- server/gqlserver.js
+++ server/gqlserver.js
@@ -1,5 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
-const Query = require('./gql/Queries.js');
+const resolvers = require('./gql/Queries.js');
 const Mutation = require('./gql/Mutations.js');
 const Subscription = require('./gql/Subscriptions.js');
 const db = require('./../db/index.js');
@@ -11,42 +11,6 @@ db.connect((err) => {
     console.log('connected to db')
   }
 })
-
-const resolvers = {
-  Query,
-  Staff: {
-    helpdesks({staff_slack_id}) {
-      console.log(staff_slack_id);
-      return db.query({
-        text: 'SELECT * FROM helpdesk WHERE staff_slack_id = $1',
-        values: [staff_slack_id],
-      }) 
-        .then(result => {
-          console.log(result.rows);
-          return result.rows;
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    },
-    helpdeskCount({staff_slack_id}) {
-      console.log(staff_slack_id);
-      return db.query({
-        text: 'SELECT COUNT(*) FROM helpdesk WHERE staff_slack_id = $1',
-        values: [staff_slack_id],
-      }) 
-        .then(result => {
-          return result.rows[0].count;
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    },
-
-  },
-  // Mutation,
-  // Subscription
-}
 
 const server = new GraphQLServer({ 
   typeDefs : './server/gql/schema.graphql',
