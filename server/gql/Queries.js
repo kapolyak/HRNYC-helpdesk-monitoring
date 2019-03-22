692f8c554d34529be35d91755fa8695841135b64
Kenny <kennethpolyak@gmail.com>
2019-03-21 23:12:30 -0400

    Code cleanup

--- server/gql/Queries.js
+++ server/gql/Queries.js
@@ -134,105 +134,14 @@ function countPerDay (parent, args, context, info) {
 }
 
 function allHelpRequests (parent, args, context, info) {
-  // let sql = args.cohort_number ? 'SELECT * FROM helpdesk' : 'SELECT * FROM helpdesk'
 
-  if (args.cohort_number) {
-    console.log('cohort number', args.cohort_number);
-
-    return context.db.query(
-      `
-      SELECT * FROM cohort
-      WHERE cohort.cohort_number = '${args.cohort_number}'
-      `
-    ).then(cohortData => {
-      return context.db.query(
-        `
-        SELECT helpdesk.student_name, student.cohort_number, helpdesk.opened_ts from helpdesk 
-        INNER JOIN student 
-        ON student.student_slack_id = helpdesk.slack_id
-        WHERE student.cohort_number = '${args.cohort_number}'
-        `
-        )
-        .then(result => {
-          console.log('COHORT DATA', cohortData); 
-
-          const UNIX_BEGIN = Number(cohortData.rows[0].begin_unix_time);
-          const UNIX_END = Number(cohortData.rows[0].end_unix_time);
-
-          const hash = {
-            1: 0,
-            2: 0,
-            3: 0,
-            4: 0,
-            5: 0,
-            6: 0,
-            7: 0,
-            8: 0,
-            9: 0,
-            10: 0,
-            11: 0,
-            12: 0,
-            13: 0
-          }
-
-          const intervals = [
-            [UNIX_BEGIN],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            []
-          ]
-
-          // fill intervals
-          intervals.forEach((interval, i) => {
-            interval[1] = interval[0] + 604800;
-
-            if (intervals[i + 1] !== undefined) {
-              intervals[i + 1][0] = interval[1];
-            }
-          })
-
-          let tickets = result.rows;
-          
-          tickets.forEach(ticket => {
-            intervals.forEach((interval, i) => {
-              if (ticket.opened_ts > interval[0] && ticket.opened_ts < interval[1]) {
-                hash[i + 1] += 1;
-              } 
-            })
-          })
-          let results = [];
-
-          for (let key in hash) {
-            results.push([key, hash[key]]);
-          }
-          console.log('HASH ARRAY', results);
-
-          return results; 
-        })
-        .catch(err => {
-          console.log(err);
-        });
+  return context.db.query('SELECT * FROM helpdesk')
+    .then(result => {
+      return result.rows; 
     })
-
-  } else {
-    console.log('no args')
-    return context.db.query('SELECT * FROM helpdesk')
-      .then(result => {
-        return result.rows; 
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+    .catch(err => {
+      console.log(err);
+    });
 
 }
 
